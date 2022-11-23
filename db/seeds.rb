78c7aebaa8d58691b92e464c66d5292bc222b910
Dennis Njogu <dennis.njogu@student.moringaschool.com>
2022-11-23 17:10:19 +0300

    finished assignment

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Planting data...."
+Student.create(
+    [
+        {
+            first_name: "Dwayne",
+            last_name: "Johnson",
+            grade: 80
+        },
+        {
+            first_name: "Dennis",
+            last_name: "Njogu",
+            grade: 98
+        },
+        {
+            first_name: "Esther",
+            last_name: "Wanjiku",
+            grade: 90
+        },
+        {
+            first_name: "Alex",
+            last_name: "Kinuthia",
+            grade: 88
+        }
+    ]
+)
+Puts "Data planted"
